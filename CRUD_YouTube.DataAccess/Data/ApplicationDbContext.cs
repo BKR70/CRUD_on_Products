bfe62a319edf2053454deacfc467c45467a8de0c
BKR70 <bilas.duet.204027@gmail.com>
2025-06-10 13:32:29 +0600

    applicationdbcontext of identity is deleted

--- CRUD_YouTube.DataAccess/Data/ApplicationDbContext.cs
+++ CRUD_YouTube.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using CRUD_YouTube.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CRUD_YouTube.DataAccess.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
         {
